0f3293d54174d66615953598b2e2f8a5877ad89c
Urooj-Islam-Khan <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2023-03-20 12:08:01 +0500

    Add files via upload

---
+++ UsingEscapeSequences.java
@@ -0,0 +1,8 @@
+public class UsingEscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("I'm \'Urooj Islam\' \n Studen of software Engineering");
+        //  \n is used for new line \' for single quote
+        System.out.println("\"Coding is \t fun\"..\b");
+        // \" used for double quote,  \t for tab
+    }
+}
