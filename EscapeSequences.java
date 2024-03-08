13bb62111f5142921ba81469cb274b9e8f1157f7
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-03-08 14:23:28 +0500

    Add files via upload

---
+++ EscapeSequences.java
@@ -0,0 +1,10 @@
+package Basic_Programs;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("I'm \'Urooj Islam\' \n Studen of software Engineering");
+        //  \n is used for new line \' for single quote
+        System.out.println("\"Coding is \t fun\"..\b");
+        // \" used for double quote,  \t for tab
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+package Basic_Programs;
+
+public class HelloWorld {
+    public static void main(String[] args)  // main method
+    {
+
+        //System.out.println() method to print message
+
+        System.out.println("Hello World...");
+    }
+}
+
+// is used to comment
+
+/*public static void main(String[] args) :
+ definesa static main method which takes an array of string as an argument.
+*/
+
+
+//System.out.println("Hello World..."); use to print message.
+
