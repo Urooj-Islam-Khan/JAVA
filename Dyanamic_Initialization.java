daa329ab6609a1a014bbd4b5e926d1247ae2f17f
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-03-08 14:22:00 +0500

    Add files via upload

---
+++ Dyanamic_Initialization.java
@@ -0,0 +1,13 @@
+package Basic_Programs;
+
+import java.util.Scanner;
+
+public class Dyanamic_Initialization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers ");
+        double a= sc.nextDouble(), b = sc.nextDouble();
+        double c = Math.sqrt(a*a + b*b);
+        System.out.println("Hypotenus of c is "+ c);
+    }
+}
